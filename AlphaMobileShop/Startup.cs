bd75e701b6e4011b23d9e8ae8f236152bc4f190d
Hristijan.Tonchevski <Hristijan.Tonchevski@teaming.inteligenta.io>
2022-07-21 20:16:15 +0200

    Add project files.

---
+++ AlphaMobileShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlphaMobileShop.Startup))]
+namespace AlphaMobileShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
